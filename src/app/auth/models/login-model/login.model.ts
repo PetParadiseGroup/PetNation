af09c50ce91fd375156389c2f774b644df98160f
Ian <u202214059@upc.edu.pe>
2024-06-21 23:21:04 -0500

    Home Publications

---
+++ src/app/auth/models/login-model/login.model.ts
@@ -0,0 +1,9 @@
+export class Login {
+  correoElectronico: string;
+  contrasenia: string;
+
+  constructor(correoElectronico: string, contrasenia: string) {
+    this.correoElectronico = correoElectronico;
+    this.contrasenia = contrasenia;
+  }
+}
